b116ccacaa175cb9c38d2be98d783994a978b0a9
詹群 <zhanqun@eccom.com.cn>
2019-08-29 18:10:05 +0800

    组件demo

--- src/pages/module1/Page.js
+++ src/pages/module1/Page.js
@@ -9,6 +9,7 @@ import styles from './index.less';
 const {
   Layouts: { withBasicLayout },
   SelectFields: { SelectFieldOffice, SelectFieldEmployeeTypes, SelectFieldPersonTypes },
+  InputFields: { InputFieldPerson, InputFieldPersonMulti },
 } = Components;
 
 @withBasicLayout({
@@ -22,20 +23,26 @@ class Template extends React.Component {
   render() {
     return (
       <div className={styles.container}>
-        <div>
+        <div className={styles.item}>
           <Link to="/index">Welcome</Link>
         </div>
-        <div>
+        <div className={styles.item}>
           <Link to="/module1/page2/template3">HideInMenu</Link>
         </div>
-        <div>
+        <div className={styles.item}>
           <SelectFieldOffice />
         </div>
-        <div>
+        <div className={styles.item}>
           <SelectFieldEmployeeTypes />
         </div>
-        <div>
+        <div className={styles.item}>
           <SelectFieldPersonTypes />
+        </div>
+        <div className={styles.item}>
+          <InputFieldPerson allowClear value="ZhanQun82608" />
+        </div>
+        <div className={styles.item}>
+          <InputFieldPersonMulti allowClear value="wwz;jacky" />
         </div>
       </div>
     );
