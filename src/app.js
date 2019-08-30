9b7123749315b5c412850a3eb97aefd776953b7e
詹群 <zhanqun@eccom.com.cn>
2019-08-30 11:08:01 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ export function patchRoutes(routes) {
   console.log('patchRoutes', routes);
 }
 
+// 可在此增加页面访问追踪
 export function onRouteChange({ location, routes, action }) {
   console.log('onRouteChange', location, routes, action);
 }
